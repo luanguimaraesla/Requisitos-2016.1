b38f388b0c27f1385c9ecb669e165ecdcecfe0bc
Luan Guimarães <luan@live.in>
2016-06-19 00:06:50 -0300

    Configure devise

--- client-management-tools/app/controllers/application_controller.rb
+++ client-management-tools/app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :update_sanitized_params, if: :devise_controller?
+  def update_sanitized_params
+    devise_parameter_sanitizer.for(:sign_up) do |u|
+      u.permit(:email, :password, :password_confirmation, :username, :name, :cpf, :phone, :company, :address)
+    end
+
+    devise_parameter_sanitizer.for(:account_update) do |u|
+      u.permit(:email, :password, :password_confirmation, :username, :name, :phone, :company, :address)
+    end
+  end
 end
--- client-management-tools/config/routes.rb
+++ client-management-tools/config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :clients
+  devise_for :clients, controllers: { sessions: 'clients/sessions' }
   resources :services
   resources :products
+  root 'products#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
