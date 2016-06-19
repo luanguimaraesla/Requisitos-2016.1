86488975df8fa5a28dd94f7fbe52b9b0436c44c2
Luan Guimarães <luan@live.in>
2016-06-19 01:12:03 -0300

    Add profile picture to clients

---
+++ client-management-tools/db/migrate/20160619035714_add_picture_to_client.rb
@@ -0,0 +1,8 @@
+class AddPictureToClient < ActiveRecord::Migration
+  def up
+    add_attachment :clients, :profile_picture
+  end
+  def down
+    remove_attachment :clients, :profile_picture
+  end
+end
