563e4120f7c9fb82af441fd3b51817215dccc738
Luan Guimarães <luan@live.in>
2016-06-28 21:31:47 -0300

    Add the integration between orders and clients.

--- client-management-tools/app/controllers/clients_controller.rb
+++ client-management-tools/app/controllers/clients_controller.rb
@@ -1,5 +1,11 @@
 class ClientsController < ApplicationController
   def show
     @client = Client.find(params[:id])
+    @orders_counter = @client.orders.count
+  end
+
+  def my_orders
+    @client = Client.find(params[:id])
+    @orders = @client.orders
   end
 end
